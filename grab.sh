b3654c388287d535ec10c93a8e5d4c4ca13c08a5
Alex Bush <alex.pheonixtechnical@gmail.com>
2022-01-09 19:47:30 +0000

    feat: fix script

--- grab.sh
+++ grab.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 /usr/sbin/lighttpd -D -f /lighttpd.conf &
 if [ -n "$URL1" ]; then
-  url1=$URL1
-  ffmpeg -loglevel panic -y -i $url1 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot1.jpg
+  ffmpeg -rtsp_transport tcp -i '$URL1' -q:v 4 -r 1 -update 1 /tmp/www/snapshot1.jpg &
 fi
 if [ -n "$URL2" ]; then
-url2=$URL2
-ffmpeg -loglevel panic -y -i $url2 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot2.jpg
+  ffmpeg -rtsp_transport tcp -i '$URL2' -q:v 4 -r 1 -update 1 /tmp/www/snapshot2.jpg &
 fi
 if [ -n "$URL3" ]; then
-url3=$URL3
-ffmpeg -loglevel panic -y -i $url3 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot3.jpg
+  ffmpeg -rtsp_transport tcp -i '$URL3' -q:v 4 -r 1 -update 1 /tmp/www/snapshot3.jpg &
 fi
 if [ -n "$URL4" ]; then
-url4=$URL4
-ffmpeg -loglevel panic -y -i $url4 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot3.jpg
+  ffmpeg -rtsp_transport tcp -i '$URL4' -q:v 4 -r 1 -update 1 /tmp/www/snapshot4.jpg &
 fi
 echo "Started all, waiting"
 tail -f /dev/null
