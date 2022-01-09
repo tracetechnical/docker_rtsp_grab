4a97ab0da79ad3e36bbb3af0abe46e9cbd4cc66a
Alex Bush <alex.pheonixtechnical@gmail.com>
2022-01-09 18:58:34 +0000

    feat: update to add more urls

--- grab.sh
+++ grab.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 /usr/sbin/lighttpd -D -f /lighttpd.conf &
-url=$URL
-ffmpeg -loglevel panic -y -i $url -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot.jpg
+if [ -n "$URL1" ]
+  url1=$URL1
+  ffmpeg -loglevel panic -y -i $url1 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot1.jpg
+fi
+if [ -n "$URL2" ]
+url2=$URL2
+ffmpeg -loglevel panic -y -i $url2 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot2.jpg
+fi
+if [ -n "$URL3" ]
+url3=$URL3
+ffmpeg -loglevel panic -y -i $url3 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot3.jpg
+fi
+if [ -n "$URL4" ]
+url4=$URL4
+ffmpeg -loglevel panic -y -i $url4 -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot3.jpg
+fi
