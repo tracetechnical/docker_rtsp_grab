5fc23f65300fcdbac6b3877ca641536c3cd43af2
Alex Bush <alex.pheonixtechnical@gmail.com>
2022-01-10 16:03:43 +0000

    feat: remove rtsp option

--- grab.sh
+++ grab.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 /usr/sbin/lighttpd -D -f /lighttpd.conf &
 if [ -n "$URL1" ]; then
-  ffmpeg -rtsp_transport tcp -i "$URL1" -q:v 25 -r 1 -update 1 /tmp/www/snapshot1.jpg &
+  ffmpeg -i "$URL1" -q:v 25 -r 1 -update 1 /tmp/www/snapshot1.jpg &
 fi
 if [ -n "$URL2" ]; then
-  ffmpeg -rtsp_transport tcp -i "$URL2" -q:v 25 -r 1 -update 1 /tmp/www/snapshot2.jpg &
+  ffmpeg -i "$URL2" -q:v 25 -r 1 -update 1 /tmp/www/snapshot2.jpg &
 fi
 if [ -n "$URL3" ]; then
-  ffmpeg -rtsp_transport tcp -i "$URL3" -q:v 25 -r 1 -update 1 /tmp/www/snapshot3.jpg &
+  ffmpeg -i "$URL3" -q:v 25 -r 1 -update 1 /tmp/www/snapshot3.jpg &
 fi
 if [ -n "$URL4" ]; then
-  ffmpeg -rtsp_transport tcp -i "$URL4" -q:v 25 -r 1 -update 1 /tmp/www/snapshot4.jpg &
+  ffmpeg -i "$URL4" -q:v 25 -r 1 -update 1 /tmp/www/snapshot4.jpg &
 fi
 echo "Started all, waiting"
 tail -f /dev/null
