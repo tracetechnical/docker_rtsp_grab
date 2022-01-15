e69f7bfe9f26d7c78b2a52799251e01a3e78b4b8
Alex Bush <alex.pheonixtechnical@gmail.com>
2022-01-15 20:57:40 +0000

    feat: add quality variable; feat: up retry counter

--- grab.sh
+++ grab.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 /usr/sbin/lighttpd -D -f /lighttpd.conf &
+if [ -n "QUALITY" ]; then
+  Q = $QUALITY
+else
+  Q = 25
+fi
 if [ -n "$URL1" ]; then
-  ffmpeg -i "$URL1" -q:v 25 -r 1 -update 1  -loglevel quiet -stats -y /tmp/www/snapshot1.jpg &
+  ffmpeg -i "$URL1" -q:v $Q -r 1 -update 1  -loglevel quiet -stats -y -max_reload 2147483647 -m3u8_hold_counters 2147483647 /tmp/www/snapshot1.jpg &
 fi
 if [ -n "$URL2" ]; then
-  ffmpeg -i "$URL2" -q:v 25 -r 1 -update 1 /tmp/www/snapshot2.jpg &
+  ffmpeg -i "$URL2" -q:v $Q -r 1 -update 1  -loglevel quiet -stats -y -max_reload 2147483647 -m3u8_hold_counters 2147483647 /tmp/www/snapshot2.jpg &
 fi
 if [ -n "$URL3" ]; then
-  ffmpeg -i "$URL3" -q:v 25 -r 1 -update 1 /tmp/www/snapshot3.jpg &
+  ffmpeg -i "$URL3" -q:v $Q -r 1 -update 1  -loglevel quiet -stats -y -max_reload 2147483647 -m3u8_hold_counters 2147483647 /tmp/www/snapshot3.jpg &
 fi
 if [ -n "$URL4" ]; then
-  ffmpeg -i "$URL4" -q:v 25 -r 1 -update 1 /tmp/www/snapshot4.jpg &
+  ffmpeg -i "$URL4" -q:v $Q -r 1 -update 1  -loglevel quiet -stats -y -max_reload 2147483647 -m3u8_hold_counters 2147483647 /tmp/www/snapshot4.jpg &
 fi
 echo "Started all, waiting"
 tail -f /dev/null
